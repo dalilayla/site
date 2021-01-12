dafd60cc8474c305a97b3efceb8a29941405fb9d
Dalila Sagingalieva <47076270+dalilayla@users.noreply.github.com>
2021-01-12 10:11:55 +0600

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,20 @@
+const url = "http://localhost:3000/users";
+const vn = new Vue({
+    el:"#app",
+    data:{
+        results:[]
+    },
+    mounted(){
+        axios.get(url).then(res=>{
+            this.results=res.data
+        })
+    },
+    methods:{
+        async deleteById(index) {
+            let id = this.results[index].id
+            this.results.splice(index,1)
+            alert('deleted ' + id)
+            await axios.delete("http://localhost:3000/users/" + id)
+        }
+    }
+});
